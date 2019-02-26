1caf849b09edd1ae6cc9dbdaec8b83cf793057e0
Henrik Helenius <henrik@codeberry.fi>
2019-02-26 17:35:21 +0200

    first commit

---
+++ data/backend.js
@@ -0,0 +1,211 @@
+// Helpers
+
+String.prototype.replaceAll = function(search, replacement) {
+	return this.replace(new RegExp(search, 'g'), replacement)
+};
+
+String.prototype.capitalize = function() {
+	return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+Array.prototype.pushIfNotExistCounter = function(element) {
+	if (JSON.stringify(this).indexOf(element) === -1) this.push({
+		name: element,
+		frequency: 1
+	});
+	else this.find(object => object.name === element).frequency += 1
+};
+
+function makeJSON(data, resultName) {
+	const verified = [];
+	const unverified = [];
+	data.filter(item => item['Valikoima'] !== 'tarvikevalikoima')
+			.filter(item => item['Tyyppi'] !== 'lahja- ja juomatarvikkeet')
+			.filter(item => !!item['Alkoholi-%'] && item['Alkoholi-%'] !== '0.0')
+			.filter(item => !!item['Pullokoko'])
+			.filter(item => !!item['Nimi'])
+			.filter(item => !!item['Hinta'])
+			.forEach(item => {
+				const name = item['Nimi'];
+				const liters = parseFloat(item['Pullokoko'].split(' ')[0].replace(',', '.'));
+				const alcoholPercent = parseFloat(item['Alkoholi-%']);
+				const alcoholAbsolute = liters * alcoholPercent;
+				const price = parseFloat(item['Hinta']);
+				const euroPerAlcohol = price / alcoholAbsolute;
+				const urlSlug = name.toLowerCase()
+					.replaceAll('\'', '')
+					.replaceAll(' ', '-')
+					.replace(new RegExp(/[àáâãäåö]/g),"-");
+				const completeUrlGuess = 'https://www.alko.fi/tuotteet/' + item['Numero'] + '/' + urlSlug;
+				const nav = 'Not available';
+
+				const final = {
+					euroPerAlcohol: euroPerAlcohol,
+					url:            completeUrlGuess,
+					id:             item['Numero' || nav],
+					name:           name,
+					producer:       item['Valmistaja'] || nav,
+					liters:         liters,
+					price:          price,
+					newInStock:     item['Uutuus'] || nav,
+					type:           item['Tyyppi'] || nav,
+					speciality:     item['Erityisryhmä'] || nav,
+					country:        item['Valmistusmaa'] || nav,
+					subregion:      item['Alue'] || nav,
+					year:           item['Vuosikerta'] || nav,
+					package:        item['Pakkaustyyppi'] || nav,
+					closingMethod:  item['Suljentatyppi'] || nav,
+					alcoholPercent: alcoholPercent,
+					sugarGramPerL:  item['Sokeri g/l'] || nav,
+					energyPer100:   item['Energia kcal/100 ml'] || nav,
+					availability:   item['Valikoima'] || nav
+				};
+
+				const literPrice = item['Litrahinta'];
+				const calculatedLiterPrice = price / liters;
+				const literPriceCheck = (literPrice - calculatedLiterPrice) < 1;
+
+				if (literPriceCheck) verified.push(final); else unverified.push(final);
+			});
+
+	const filtering = (items) => {
+		const f = {
+			types: [],
+			countries: [],
+			packages: [],
+			closingMethods: [],
+			availabilities: [],
+			specialities: [],
+			newInStocks: []
+		};
+
+		items.forEach( item => {
+			f.types.pushIfNotExistCounter(item.type.capitalize());
+			f.countries.pushIfNotExistCounter(item.country.capitalize());
+			f.packages.pushIfNotExistCounter(item.package.capitalize());
+			f.closingMethods.pushIfNotExistCounter(item.closingMethod.capitalize());
+			f.availabilities.pushIfNotExistCounter(item.availability.capitalize());
+			f.specialities.pushIfNotExistCounter(item.speciality.capitalize());
+			f.newInStocks.pushIfNotExistCounter(item.newInStock.capitalize());
+		});
+
+		return f
+	};
+
+	const ratioSort = arr => arr.sort((first, second) => first.euroPerAlcohol - second.euroPerAlcohol);
+	const ranker = (item, rank) => { item['rank'] = rank + 1; return item };
+	const alla = {
+		verified: {
+			items: ratioSort(verified).map((item, index) => ranker(item, index)),
+			filtering: filtering(verified)
+		},
+		unverified: {
+			items: ratioSort(unverified).map((item, index) => ranker(item, index)),
+			filtering: filtering(unverified)
+		}
+	};
+
+	const fs = require('fs');
+	fs.writeFile(resultName, 'var data = ' + JSON.stringify(alla/*, null, 3*/), 'utf8', () => {
+		console.log('JSON created successfully. Verified: ' + verified.length + ' unverified: ' + unverified.length)
+	});
+}
+
+const convertToJson = (name) => {
+	const excelToJson = require('convert-excel-to-json');
+	return excelToJson({
+		sourceFile: name,
+		columnToKey: {
+			A: 'Numero',
+			B: 'Nimi',
+			C: 'Valmistaja',
+			D: 'Pullokoko',
+			E: 'Hinta',
+			F: 'Litrahinta',
+			G: 'Uutuus',
+			H: 'Hinnastojärjestyskoodi',
+			I: 'Tyyppi',
+			J: 'Erityisryhmä',
+			K: 'Oluttyyppi',
+			L: 'Valmistusmaa',
+			M: 'Alue',
+			N: 'Vuosikerta',
+			O: 'Etikettimerkintöjä',
+			P: 'Huomautus',
+			Q: 'Rypäleet',
+			R: 'Luonnehdinta',
+			S: 'Pakkaustyyppi',
+			T: 'Suljentatyppi',
+			U: 'Alkoholi-%',
+			V: 'Hapot g/l',
+			W: 'Sokeri g/l',
+			X: 'Kantavierrep-%',
+			Y: 'Väri EBC',
+			Z: 'Katkerot EBU',
+			AA: 'Energia kcal/100 ml',
+			AB: 'Valikoima'
+		}
+	})
+};
+
+const downloadXls = (name) => {
+	const url = 'https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona.xls';
+	const https = require('https');
+	const fs = require('fs');
+
+	const file = fs.createWriteStream(name);
+	https.get(url, (response) => {
+		response.pipe(file);
+		file.on('finish', () => {
+			file.close(jsonProcess);
+		});
+	});
+};
+
+const date = new Date();
+const xlsName = `data_${date.toISOString()}.xls`;
+
+const process = () => {
+	downloadXls(xlsName);
+};
+
+const jsonProcess = (name = xlsName) => {
+	const data = convertToJson(name);
+	const sheet1Data = data[Object.keys(data)[0]];
+	console.log(sheet1Data, typeof data);
+	makeJSON(sheet1Data, '../data.js');
+};
+
+process();
+
+/////////////////////////
+////// Depreciated //////
+/////////////////////////
+
+/*
+function google(product) {
+  function getJSONP(url, success) {
+    var ud = '_' + +new Date,
+      script = document.createElement('script'),
+      head = document.getElementsByTagName('head')[0] ||
+      document.documentElement;
+
+    window[ud] = function(data) {
+      head.removeChild(script);
+      success && success(data);
+    };
+    script.src = url.replace('callback=?', 'callback=' + ud);
+    head.appendChild(script);
+  }
+
+  getJSONP('https://www.googleapis.com/customsearch/v1?key=AIzaSyCXEzHsFuu8UX7l-qDBNGpEjhFX6Cv4rlE&cx=002030677678250887662:wcrnljldml0&q=' + product + '&callback=?', function(data) {
+    const siteUrl = data.items[0].formattedUrl
+    const imgUrl = data.items[0].pagemap.cse_image[0].src
+    const imgUrlThumb = data.items[0].pagemap.cse_thumbnail[0].src
+    sources = {
+      site: siteUrl,
+      img: imgUrl,
+      thumb: imgUrlThumb
+    }
+  });
+}*/
---
+++ js/index.js
@@ -0,0 +1,226 @@
+let savedSex = 1;
+let savedWeight = 80;
+let hoursBurning = 0.15;
+
+let filters = {
+	minPrice: 0,
+	maxPrice: 0,
+	alcoholPercent: 0,
+	energyPer100: 0,
+	type: '',
+	/*package: '',
+	closingMethod: '',
+	name: '',
+	producer: '',
+	liters: 0,
+	newInStock: '',
+	country: '',
+	subregion: '',
+	year: '',
+	availability: ''*/
+};
+
+let currentLoaded = 50;
+let loadMoreAvailable = true;
+
+const filterList = (itemsInView = 50) => {
+	const filtering = function ( item ) {
+		//var priceControl = item.price <= filters.maxPrice
+		const l = s => s.toLowerCase();
+		let typeControl = l(filters.type) == 'kaikki' || l(item.type) == l(filters.type) || !filters.type.length ;
+		let packageControl = !filters.package || l(filters.package).includes(l(item.package));
+		return typeControl && packageControl
+	};
+
+	if (itemsInView === 50) currentLoaded = 50; else currentLoaded = itemsInView;
+
+	const filtered = data.verified.items.filter( item => filtering(item));
+
+	// Restrict list size
+
+	/*
+	const startSlice = filtered.slice(0, itemsInView / 2);
+	const size = filtered.length;
+	const tailSlice = filtered.slice(size - (itemsInView / 2) - 1, size - 1);
+	const sliced = startSlice.concat( tailSlice );
+	return sliced
+	*/
+	const returnable = filtered.slice(0, itemsInView);
+	loadMoreAvailable = (returnable.length >= 1);
+	return returnable;
+};
+
+const makeList = current => {
+	const listContainer = document.querySelector('.product-grid');
+	let markup = '';
+	current.map( item => {
+		const slicedRatio = item.euroPerAlcohol.toString().slice(0,4);
+		const formatedRatio = slicedRatio[3] == '.' ? slicedRatio.slice(0, 3) : slicedRatio;
+		markup +=  `
+            <div class="product">
+                <div class="product__info">
+                    <!--  Euros per alcohol: ${ item.euroPerAlcohol } -->
+                    <div class="badge-box outer">
+                        <div class="badge-box-text">${ formatedRatio }</div>
+                        <div class="badge-box"><div class="badge-box"><div class="badge-box"></div></div></div>
+                        <span class="action__text action__text--invisible">Hinta per alkoholiyksikkö</span>
+                    </div>
+                    <img class="product__image" src="" alt="${ item.name }" style="display: none;" />
+                    <div class="rank" data-length="${ item.rank.toString().length }">${ item.rank }</div>
+                    <h3 class="product__title">${ item.name }</h3>
+                    <span class="product__year extra highlight">${ item.type }</span>
+                    <span class="product__region extra highlight">${ item.energyPer100 } kcal / 100ml & ${ item.alcoholPercent }%</span>
+                    <span class="product__varietal extra highlight">${ item.package } </span>
+                    <span class="product__alcohol extra highlight">${ item.closingMethod }</span>
+                    <span class="product__price highlight">${ item.price }€</span>
+                    <a href="${ item.url }" target="_blank">
+                        <button class="action action--button action--buy">
+                            <div class="alko-logo"></div>
+                            <!--<i class="alko-icon alko-icon-shopping-cart"></i><span class="action__text">Buy at Alko</span>-->
+                        </button>
+                    </a>
+                </div>
+                <label class="action action--compare-add"><input class="check-hidden" type="checkbox" /><i class="alko-icon alko-icon-plus"></i><i class="alko-icon alko-icon-check"></i><span class="action__text action__text--invisible">Lisää vertailuun</span></label>
+            </div>
+        `;
+	});
+	listContainer.innerHTML = markup;
+	makeLoadMore();
+	window.reinit()
+};
+
+const makePromilles = () => {
+
+};
+
+const makeFilters = ( items ) => {
+	const countFiltering = (items) => {
+		Array.prototype.pushIfNotExistCounter = function(element) {
+			if (JSON.stringify(this).indexOf(element) == -1) this.push({
+				name: element,
+				frequency: 1
+			});
+			else this.find(object => object.name === element).frequency += 1
+		};
+
+		String.prototype.capitalize = function() {
+			return this.charAt(0).toUpperCase() + this.slice(1);
+		};
+
+		const f = {
+			types: [],
+			countries: [],
+			packages: [],
+			closingMethods: [],
+			availabilities: [],
+			specialities: [],
+			newInStocks: []
+		};
+
+		items.forEach( item => {
+			f.types.pushIfNotExistCounter(item.type.capitalize());
+			f.countries.pushIfNotExistCounter(item.country.capitalize());
+			f.packages.pushIfNotExistCounter(item.package.capitalize());
+			f.closingMethods.pushIfNotExistCounter(item.closingMethod.capitalize());
+			f.availabilities.pushIfNotExistCounter(item.availability.capitalize());
+			f.specialities.pushIfNotExistCounter(item.speciality.capitalize());
+			f.newInStocks.pushIfNotExistCounter(item.newInStock.capitalize());
+		});
+
+		return f
+	};
+
+	const makeOptionsMarkup = (options) => {
+		let markup = '';
+		options.forEach( option => markup += `<option value="${ option.name }">${ option.name }</option>` );
+		return markup
+	};
+	const filtersEl = document.querySelector('.filters');
+	// Type filtering
+	const typeSelect = filtersEl.querySelector('#type');
+	const availableCategories = countFiltering(items).types.filter(o => o.frequency >= 1);
+	typeSelect.innerHTML = makeOptionsMarkup(
+		[
+			{
+				name: 'Kaikki',
+				frequency: 1
+			},
+			...availableCategories
+		]
+	)
+};
+
+const makeLoadMore = () => {
+	const el = document.querySelector('.load-more-container');
+	if (loadMoreAvailable) {
+		el.innerHTML = `
+			<button class="load-more" onclick="loadMore()">
+				Näytä 50 seuraavaa
+			</button>
+		`
+	} else {
+		el.innerHTML = '';
+		document.querySelector('.product-grid').innerHTML = `
+			<p class="loading-text">Emme löytäneet sopivaa tuotetta</p>
+		`;
+	}
+};
+
+const setFilter = (el) => {
+	filters[el.id] = el.value;
+	emptyBasket();
+	updateList();
+};
+
+const promilles = (alcoholPercentage, mass = savedWeight, sex = savedSex, burning = hoursBurning) => { // sex; 1: M, 0: F
+	const density = 0.79;
+	const alcMl = alcoholPercentage * 1000;
+	const alcToGram = alcMl*density;
+	const massToVol = mass * 1000;
+	let times = 0;
+	if (!!sex) times = 0.68; else times = 0.55;
+	let BAC = (alcToGram/ ( massToVol*times )) - (0.00015 * burning);
+	let promille = BAC * 1000;
+	return promille
+};
+
+const listenSwitchFilterClicks = () => {
+	document.querySelectorAll('.switches input + label').forEach(function(label) {
+		label.addEventListener('click', function(ev) {
+			const forValue = ev.target.htmlFor;
+			const radio = document.querySelector('.switches input:checked');
+			if ( radio && radio.id === forValue ) {
+				ev.preventDefault();
+				radio.checked = false;
+				setFilter({
+					id: 'package',
+					value: ''
+				})
+			} else {
+				const value = document.querySelector(`.switches input#${forValue}`).value;
+				setFilter({
+					id: 'package',
+					value: value
+				})
+			}
+		});
+	});
+};
+
+const emptyBasket = () =>
+	document.querySelectorAll('.action--remove').forEach(e => e.click());
+
+const loadMore = () =>
+	makeList( filterList(currentLoaded + 50) );
+
+const updateList = () => makeList( filterList() );
+
+const initialise = () => {
+	if (!!data) {
+		listenSwitchFilterClicks();
+		makeFilters( filterList() );
+		updateList();
+	} else {
+		setTimeout(() => initialise(), 100)
+	}
+};
